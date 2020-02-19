9d6c31c47d507b5b0478c9dabd0b0ee369cbc7e4
1MLightyears <1MLightyears@gmail.com>
2020-02-19 18:08:53 +0800

    v0.6(upgrade algorithm/overlap warning/volume preview/bugfix)

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 ﻿# Severus
-A Chinese small software to combine/split a RAR archive with a picture(JPG/GIF)
+A zh-CN small software to combine/split a RAR archive with a picture file(JPG/PNG/GIF)
 
 这个轻量级的应用是用来制作图种的。<br>
 通过这个应用，你可以<br>
-\-通过将一个图片文件(JPG/GIF)和一个.rar文件头尾相接，将他们合成一个文件，同时还能正常地打开并看到图片<br>
+\-通过将一个图片文件(JPG/PNG/GIF)和一个RAR压缩文件头尾相接，将他们合成一个文件，同时还能正常地打开并看到图片！<br>
 也可以<br>
-\-用WinRAR解压缩一个图种文件，并把里面的内容释放出来<br>
-只需运行Severus.exe即可！
+\-用WinRAR解压缩一个图种文件，并把里面的内容释放出来！<br>
+（提醒：可能由于相机/手机拍摄的照片通常含有关于拍摄的附加信息的原因，以这些图片制作的图种可能无法正常解包，请确认图片来源。）<br>
+
 
 <br><br>
 ---
-Patch List:<br>
+这个程序运行在Windows 10(x64)上。<br>
+更新内容Patch List:<br>
 v0.2<br>
 \-增加了对中文路径名的支持
 <br>
@@ -23,6 +25,11 @@ v0.4<br>
 \-现在可以制作和解压GIF格式的图种了<br>
 v0.41<br>
 \-修复了一个bug，该bug使得GIF动图无法拖入程序打开<br>
-\-修复了一个bug，该bug使得当文件扩展名是大写的时候无法拖入程序打开
+\-修复了一个bug，该bug使得当文件扩展名不规范时无法拖入程序打开<br>
 v0.5<br>
 \-现在可以制作和解压PNG格式的图种了<br>
+v0.6<br>
+\-现在在打包新图种时若可能覆盖同名文件时会提示用户了<br>
+\-现在可以预览图种的体积了<br>
+\-现在采用了更合理的方法判断图片类型<br>
+\-图种判断正确率提高了<br>
--- src/backup/main.pas
+++ src/backup/main.pas
@@ -5,7 +5,7 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Classes, SysUtils, LazFileUtils, Forms, Controls, Graphics, Dialogs, StdCtrls,
   ComCtrls, ExtCtrls, ExtDlgs, Windows, Registry, shellAPI;
 
 const
@@ -36,7 +36,7 @@ type
     Label3: TLabel;
     rarfile: TEdit;
     preview: TImage;
-    jpgfile: TEdit;
+    picfile: TEdit;
     mainpage: TPageControl;
     encode: TTabSheet;
     decode: TTabSheet;
@@ -45,17 +45,21 @@ type
     procedure Button3Click(Sender: TObject);
     procedure Button4Click(Sender: TObject);
     procedure Button5Click(Sender: TObject);
+    procedure DopenClose(Sender: TObject);
+    procedure DsaveClose(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
     procedure FormDropFiles(Sender: TObject; const FileNames: array of String);
-
+    procedure picfileChange(Sender: TObject);
+    procedure rarfileChange(Sender: TObject);
   private
-
+    filepath:unicodestring;
   public
-//    procedure DragFileProc(var Msg: TMessage);message WM_DROPFILES;
+    function EstiSize:string;
+    function FileHasRAREnd(Path:unicodestring):boolean;
   end;
   //begin DropFiles
-  TChangeWindowMessageFilter = function(msg: Cardinal; Action: Dword): BOOL; stdcall;
+  TChangeWindowMessageFilter = function(msg: Cardinal; Action: Dword): longbool; stdcall;
   //end DropFiles
 var
   //begin DropFiles
@@ -66,24 +70,71 @@ var
 implementation
 
 {$R *.lfm}
+//返回估计图种的体积
 
 { Tmainform }
+function Tmainform.EstiSize:string;
+//估计图种体积
+var
+  b:int64;
+  k,m,g:double;//Bytes, KiB, MiB, GiB(虽然我不认为会用到这么大的)
+begin
+  button3.caption:='生成图种(&G)';//完成后提示语改变，这里调回来
+  if Fileexistsutf8(picfile.text)and(Fileexistsutf8(rarfile.text))then
+    begin
+      b:=filesizeutf8(picfile.Text)+filesizeutf8(rarfile.text);
+      k:=b/1024;
+      m:=b/1048576;
+      g:=b/1074741824;
+      if g>1 then EstiSize:='(估计大小: '+floattostr(round(g*100)/100)+' GB)' else
+      if m>1 then EstiSize:='(估计大小: '+floattostr(round(m*100)/100)+' MB)' else
+         if k>1 then EstiSize:='(估计大小: '+floattostr(round(k*100)/100)+' KB)' else
+            EstiSize:='(估计大小: '+inttostr(b)+' B)'
+    end else EstiSize:='';
+end;
+
+function Tmainform.FileHasRAREnd(path:unicodestring):boolean;
+var
+  f:THandle;
+  ch:array[1..7]of char;
+  i:integer;
+begin
+  f:=fileopen(path,fmOpenRead);
+  fileseek(f,-7,2);
+  fileread(f,ch,7);
+  fileclose(f);
+  FileHasRAREnd:=false;
+  if (ch=#196#61#123#0#64#7#0)
+  or (ch=#82#97#114#33#26#7#0)
+  or (ch=#119#86#81#3#5#4#0)then
+    FileHasRAREnd:=true;
+end;
 
 procedure Tmainform.Button1Click(Sender: TObject);
 begin
   Dopen.Filter:=cPictureFilterList;
   if Dopen.Execute then begin//如果确实选取了一个文件名
-     jpgfile.Text:=Dopen.FileName;
-     preview.Picture.LoadFromFile(Dopen.FileName);//图片预览更新
+     picfile.Text:=filepath;
+     preview.Picture.LoadFromFile(filepath);//图片预览更新
   end;
   dsave.FilterIndex:=dopen.FilterIndex;
 end;
 
 procedure Tmainform.Button2Click(Sender: TObject);
+var s:string;
+  f:Thandle;
+  ch:array[1..7]of char;
+  i:integer;
 begin
   Dopen.Filter:='RAR文件|*.rar';
   //如果确实选取了一个文件名
-  if Dopen.Execute then rarfile.Text:=Dopen.FileName;
+  if Dopen.Execute then rarfile.Text:=filepath;
+  {f:=fileopen(filepath,fmOpenRead);
+  fileseek(f,-7,2);
+  fileread(f,ch,7);
+  fileclose(f);
+  s:='';for i:=1 to 7 do s:=s+inttostr(ord(ch[i]))+',';
+  showmessage(s);}
 end;
 
 procedure Tmainform.Button3Click(Sender: TObject);
@@ -96,39 +147,44 @@ var
   ans:unicodestring;
   CmdLine,Para:pwidechar;
 begin
-  if (jpgfile.text='') or (rarfile.text='') then
-    showmessage('缺少图像文件或rar压缩文件，无法打包')
-  else if Dsave.Execute then begin//如果指定了打包文件名
-      targetfile.text:=dsave.filename;//更新打包文件名显示
-      //文件名含有非法字符检测
-      ok:=true;
-      for i:=1 to 7 do if pos(targetfile.Text,ErrorNameList[i])<>0 then begin
-        ok:=false;
-        showmessage('文件名包含非法字符:/\#*&%|');
-        break;
-      end;
-      //如果没有非法字符
-      if ok then begin
-        CmdLine:=pwidechar('cmd.exe');
-        //参数表
-        ans:='/c copy /b "'+jpgfile.text+'"+"'+rarfile.text+'" "'+targetfile.text+'"';
-        //Para转化其为pwidechar格式给shellexecutew调用
-        Para:=pwidechar(ans);
-        ret:=shellexecutew(FindWindow(nil,PChar(mainform.Caption)),nil,CmdLine,Para,pwidechar(getcurrentdir),SW_HIDE);
-        //处理返回值
-        case ret of
-          ERROR_FILE_NOT_FOUND:ans:='指定的文件没有找到';
-          ERROR_PATH_NOT_FOUND:ans:='指定的地址没有找到';
-          ERROR_BAD_FORMAT:ans:='EXE文件是一个无效的PE文件格式，或者EXE文件损坏了';
-          SE_ERR_ASSOCINCOMPLETE:ans:='文件关联无效';
-          SE_ERR_DDEBUSY:ans:='DDE事物无法完成相应，因为DDE事物正在被处理';
-          SE_ERR_DDEFAIL:ans:='DDE事务失败。';
-          SE_ERR_DDETIMEOUT:ans:='DDE事务无法完成响应，因为请求超时';
-          SE_ERR_NOASSOC:ans:='没有关联程序';
-          SE_ERR_SHARE:ans:='共享越界异常';
-        else ans := '成功打包图种:'+targetfile.Text;end;//打包成功
-        showmessage(ans);
-      end;
+  if (picfile.text<>'')and(rarfile.text<>'')and(button3.caption<>'完成')then
+    if Dsave.Execute then
+      if (not Fileexists(Dsave.FileName))or((Fileexists(Dsave.FileName))and
+        (application.messagebox(
+           pchar('文件'+filepath+'已存在。'+#13#10+'要覆盖吗？'),
+           pchar('文件已存在！'),
+           MB_YesNo+MB_ICONWARNING
+           )=idyes))
+      then begin//如果指定了打包文件名
+        targetfile.text:=filepath;//更新打包文件名显示
+        //文件名含有非法字符检测
+        ok:=true;
+        for i:=1 to 7 do if pos(targetfile.Text,ErrorNameList[i])<>0 then begin
+          ok:=false;
+          showmessage('文件名包含非法字符:/\#*&%|');
+          break;
+        end;
+        //如果没有非法字符
+        if ok then begin
+          CmdLine:=pwidechar('cmd.exe');
+          //参数表
+          ans:='/c copy /b "'+picfile.Text+'"+"'+rarfile.text+'" "'+targetfile.text+'"';
+          //Para转化其为pwidechar格式给shellexecutew调用
+          Para:=pwidechar(ans);
+          ret:=shellexecutew(FindWindow(nil,PChar(mainform.Caption)),nil,CmdLine,Para,pwidechar(getcurrentdir),SW_HIDE);
+          //处理返回值
+          case ret of
+            ERROR_FILE_NOT_FOUND:showmessage('指定的文件没有找到');
+            ERROR_PATH_NOT_FOUND:showmessage('指定的地址没有找到');
+            ERROR_BAD_FORMAT:showmessage('EXE文件是一个无效的PE文件格式，或者EXE文件损坏了');
+            SE_ERR_ASSOCINCOMPLETE:showmessage('文件关联无效');
+            SE_ERR_DDEBUSY:showmessage('DDE事物无法完成相应，因为DDE事物正在被处理');
+            SE_ERR_DDEFAIL:showmessage('DDE事务失败。');
+            SE_ERR_DDETIMEOUT:showmessage('DDE事务无法完成响应，因为请求超时');
+            SE_ERR_NOASSOC:showmessage('没有关联程序');
+            SE_ERR_SHARE:showmessage('共享越界异常');
+          else button3.Caption:='完成';end;//打包成功
+        end;
     end;
 end;
 
@@ -136,8 +192,8 @@ procedure Tmainform.Button4Click(Sender: TObject);
 begin
   dopen.Filter:=cPictureFilterList;
   if dopen.Execute then begin//如果确实导入了一个图种文件
-     picname.Text:=Dopen.FileName;
-     picpreview.Picture.LoadFromFile(Dopen.FileName);
+     picname.Text:=filepath;
+     picpreview.Picture.LoadFromFile(filepath);
   end;
 end;
 
@@ -147,14 +203,13 @@ var
   s, dir: unicodeString;
   Para: PwideChar;
   ret: HINST;
-  ans: TCaption;
 begin
   //读注册表HKLM\SOFTWARE\WinRAR下exe64键值的值，得到WinRAR.exe所在位置
   reg:=Tregistry.Create(KEY_WRITE OR KEY_READ or KEY_WOW64_64KEY);
   reg.RootKey:=HKEY_LOCAL_MACHINE;
   if reg.OpenKey('SOFTWARE\WinRAR',false) then begin
-    s:=reg.ReadString('exe64');
-    if s='' then s:=reg.ReadString('exe');//如果装的是32位WinRAR
+    s:=unicodestring(reg.ReadString('exe64'));
+    if s='' then s:=unicodestring(reg.ReadString('exe'));//如果装的是32位WinRAR
     if s='' then begin
       showmessage('需要正确安装WinRAR才能解包图种内容。');
       exit;
@@ -163,31 +218,42 @@ begin
   end;
   //检查是否选取了一个图种文件
   if picname.text='' then showmessage('文件名不能为空！')else begin
-  //处理"解压到同一目录"复选框情形。被勾选的话，dir直接赋getcurrentdir。
-  if Exthere.Checked then dir:=getcurrentdir else
-    if DExt.Execute then dir:=Dext.filename else exit;
-  //如果不以\结尾，结尾补\（当不是根目录时）
-  if dir[length(dir)]<>'\' then dir:=dir+'\';
-  //一般情况下WinRAR都会在Program Files地下，有空格，因此加""
-  s:='"'+s+'"';
-  //类似的转换和调用
-  Para:=pwidechar(unicodestring(' x "'+picname.Text+'" "'+dir+'"'));
-  ret:=shellexecutew(FindWindow(nil,PChar(mainform.Caption)),nil,pwidechar(s),para,pwidechar(getcurrentdir),SW_HIDE);
-  case ret of
-          ERROR_FILE_NOT_FOUND:ans:='指定的文件没有找到';
-          ERROR_PATH_NOT_FOUND:ans:='指定的地址没有找到';
-          ERROR_BAD_FORMAT:ans:='EXE文件是一个无效的PE文件格式，或者EXE文件损坏了';
-          SE_ERR_ASSOCINCOMPLETE:ans:='文件关联无效';
-          SE_ERR_DDEBUSY:ans:='DDE事物无法完成相应，因为DDE事物正在被处理';
-          SE_ERR_DDEFAIL:ans:='DDE事务失败。';
-          SE_ERR_DDETIMEOUT:ans:='DDE事务无法完成响应，因为请求超时';
-          SE_ERR_NOASSOC:ans:='没有关联程序';
-          SE_ERR_SHARE:ans:='共享越界异常';
-        else ans := '成功解包图种:'+picname.Text;end;
-        showmessage(ans);
-  reg.Free;
+    //处理"解压到同一目录"复选框情形。被勾选的话，dir直接赋getcurrentdir。
+    if Exthere.Checked then dir:=unicodestring(getcurrentdir) else
+      if DExt.Execute then dir:=unicodestring(Dext.filename) else exit;
+    //如果不以\结尾，结尾补\（当不是根目录时）
+    if dir[length(dir)]<>'\' then dir:=dir+'\';
+    //一般情况下WinRAR都会在Program Files地下，有空格，因此加""
+    s:='"'+s+'"';
+    //类似的转换和调用
+    Para:=pwidechar(unicodestring(' x "'+picname.Text+'" "'+dir+'"'));
+    ret:=shellexecutew(FindWindow(nil,PChar(mainform.Caption)),nil,pwidechar(s),para,pwidechar(getcurrentdir),SW_HIDE);
+    case ret of
+          ERROR_FILE_NOT_FOUND:showmessage('指定的文件没有找到');
+          ERROR_PATH_NOT_FOUND:showmessage('指定的地址没有找到');
+          ERROR_BAD_FORMAT:showmessage('EXE文件是一个无效的PE文件格式，或者EXE文件损坏了');
+          SE_ERR_ASSOCINCOMPLETE:showmessage('文件关联无效');
+          SE_ERR_DDEBUSY:showmessage('DDE事物无法完成相应，因为DDE事物正在被处理');
+          SE_ERR_DDEFAIL:showmessage('DDE事务失败。');
+          SE_ERR_DDETIMEOUT:showmessage('DDE事务无法完成响应，因为请求超时');
+          SE_ERR_NOASSOC:showmessage('没有关联程序');
+          SE_ERR_SHARE:showmessage('共享越界异常');
+        end;
+    reg.Free;
   end;
 
+end;
+
+procedure Tmainform.DopenClose(Sender: TObject);
+begin
+  filepath:=dopen.FileName;
+  dopen.filename:='';
+end;
+
+procedure Tmainform.DsaveClose(Sender: TObject);
+begin
+  filepath:=Dsave.FileName;
+  Dsave.FileName:='';
 end;
 
 //begin DropFiles
@@ -223,6 +289,7 @@ const
   WM_COPYGLOBALDATA = 73;
   MSGFLT_ADD = 1;
 begin
+  randomize;mainpage.TabIndex:=0;filepath:='';
   //注册窗口可以接受拖入文件
   mainform.AllowDropFiles:=True;
   //为读取WinRAR安装路径，使用了Unit Regestry;这要求管理员权限
@@ -237,7 +304,6 @@ begin
   end;
   if paramcount>0 then
     self.FormDropFiles(self,paramstr(1));
-  mainpage.TabIndex:=0;
 end;
 
 procedure Tmainform.FormDestroy(Sender: TObject);
@@ -249,28 +315,20 @@ end;
 procedure Tmainform.FormDropFiles(Sender: TObject;
   const FileNames: array of String);
 var
-  f:Thandle;
-  ch:char;
-  i:integer;//为批量处理预留
-  s: String;
+  i:integer;//Future:为批量处理预留
+  s: unicodeString;
 begin
-  //直接跳文件尾部
   i:=0;
   s:=LowerCase(filenames[i]);
-
   if pos('.rar',s)=length(s)-3 then begin//说明.rar文件
     rarfile.Text:=s;
     mainpage.TabIndex:=0;
   end;
-
   if pos('.jpg',s)=length(s)-3 then//说明.jpg文件
     begin
-      f:=fileopen(s,fmOpenRead);
-      fileseek(f,-1,2);
-      fileread(f,ch,sizeof(ch));
-      fileclose(f);
-      if ch=#217 then begin//说明这是一个纯图片
-        jpgfile.Text:=s;
+      //直接跳文件尾部，以下均是这种逻辑
+      if not FileHasRAREnd(s) then begin//说明这是一个纯图片
+        picfile.Text:=s;
         preview.Picture.LoadFromFile(s);
         mainpage.TabIndex:=0;
         Dsave.FilterIndex:=1;
@@ -280,15 +338,10 @@ begin
         mainpage.TabIndex:=1;
       end;
     end;
-
   if pos('.png',s)=length(s)-3 then//说明.png文件
     begin
-      f:=fileopen(s,fmOpenRead);
-      fileseek(f,-1,2);
-      fileread(f,ch,sizeof(ch));
-      fileclose(f);
-      if ch=#$82 then begin//说明这是一个纯图片
-        jpgfile.Text:=s;
+      if not FileHasRAREnd(s) then begin//'IEND'块，
+        picfile.Text:=s;                                               //说明这是一个纯图片
         preview.Picture.LoadFromFile(s);
         mainpage.TabIndex:=0;
         Dsave.FilterIndex:=2;
@@ -301,12 +354,8 @@ begin
 
   if pos('.gif',s)=length(s)-3 then//说明.gif文件
      begin
-      f:=fileopen(s,fmOpenRead);
-      fileseek(f,-1,2);
-      fileread(f,ch,sizeof(ch));
-      fileclose(f);
-      if ch=#59 then begin//说明这是一个纯动图
-        jpgfile.Text:=s;
+      if not FileHasRAREnd(s) then begin//结束标记0x3B";"，说明这是一个纯动图
+        picfile.Text:=s;
         preview.Picture.LoadFromFile(s);
         mainpage.TabIndex:=0;
         Dsave.FilterIndex:=3;
@@ -317,5 +366,16 @@ begin
       end;
     end;
 end;
+
+procedure Tmainform.picfileChange(Sender: TObject);
+begin
+  button3.Caption:='生成图种'+EstiSize+'(&G)';
+end;
+
+procedure Tmainform.rarfileChange(Sender: TObject);
+begin
+  button3.Caption:='生成图种'+EstiSize+'(&G)';
+end;
+
 end.
 
